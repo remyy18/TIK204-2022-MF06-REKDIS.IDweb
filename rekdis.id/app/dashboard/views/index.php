bb7f8b764b9aa0246679afc99a4ada98fea8eab9
rizaiman <riza2001@mhs.unsyiah.ac.id>
2022-05-24 15:55:27 +0700

    Menghapus fitur ruang

--- rekdis.id/app/dashboard/views/index.php
+++ rekdis.id/app/dashboard/views/index.php
@@ -7,9 +7,6 @@ $count_dokter = mysqli_num_rows($dokter);
 $pasien = mysqli_query($conn, "SELECT * FROM pasien");
 $count_pasien = mysqli_num_rows($pasien);
 
-$ruang = mysqli_query($conn, "SELECT * FROM ruang");
-$count_ruang = mysqli_num_rows($ruang);
-
 $obat = mysqli_query($conn, "SELECT * FROM obat");
 $count_obat = mysqli_num_rows($obat);
 
@@ -48,23 +45,6 @@ $count_obat = mysqli_num_rows($obat);
         </div>
         <div class="card-content">
           <!-- <div id="pasien-chart"></div> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 col-12">
-      <div class="card">
-        <div class="card-header d-flex flex-column align-items-start pb-0">
-          <div class="avatar bg-rgba-primary p-50 m-0">
-            <div class="avatar-content">
-              <i class="feather icon-users text-primary font-medium-5"></i>
-            </div>
-          </div>
-          <h2 class="text-bold-700 mt-1 mb-25"><?= $count_ruang; ?></h2>
-          <p class="mb-0">Ruang</p>
-        </div>
-        <div class="card-content">
-          <!-- <div id="ruang-chart"></div> -->
         </div>
       </div>
     </div>
