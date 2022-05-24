356a96caf1603d4118a3ce219f8290be7ac0c5b2
rizaiman <riza2001@mhs.unsyiah.ac.id>
2022-05-24 16:04:31 +0700

    Menghapus database ruang

--- rekdis.id/app/rekam-medis/proses/create.php
+++ rekdis.id/app/rekam-medis/proses/create.php
@@ -3,14 +3,13 @@ session_start();
 require_once '../../functions/MY_model.php';
 $pasien_id = $_POST['pasien_id'];
 $dokter_id = $_POST['dokter_id'];
-$ruang_id = $_POST['ruang_id'];
 $keluhan = $_POST['keluhan'];
 $diagnosa = $_POST['diagnosa'];
 $tanggal = $_POST['tanggal'];
 $obat_id = $_POST['obat_id'];
 $created_at = date('Y-m-d H:i:s');
 $created_by = $_SESSION['user']['id'];
-$query = "INSERT INTO rekam_medis VALUES('', '$pasien_id', '$dokter_id',  '$ruang_id', '$keluhan', '$diagnosa', '$tanggal', '$created_at', '', '', '$created_by', '', '')";
+$query = "INSERT INTO rekam_medis VALUES('', '$pasien_id', '$dokter_id', '$keluhan', '$diagnosa', '$tanggal', '$created_at', '', '', '$created_by', '', '')";
 if (create($query) === 1) {
   $rm_id = mysqli_insert_id($conn);
   foreach ($obat_id as $obat) {
